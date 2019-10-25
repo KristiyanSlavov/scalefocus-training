0e005f4a14fe1257926e3a354fdfd1f80df734a4
Kristiyan Slavov <kristiyan.slavov@scalefocus.com>
2019-10-25 11:16:52 +0300

    Fix bad commit.

--- src/com/scalefocus/training/TrainingApp.java
+++ src/com/scalefocus/training/TrainingApp.java
@@ -7,30 +7,31 @@ import com.scalefocus.training.collection.tree.TreeNode;
 public class TrainingApp {
 
     public static void main(String[] args) {
-        RedBlackTree<Integer, RBNode<Integer, TreeNode<Integer>>> redBlackTree = new RedBlackTree<>();
-        redBlackTree.insert(23);
-        redBlackTree.insert(10);
-        redBlackTree.insert(33);
-        redBlackTree.insert(3);
-        redBlackTree.insert(5);
-        redBlackTree.insert(30);
-        redBlackTree.insert(35);
-
+        RedBlackTree<Integer> redBlackTree = new RedBlackTree<>();
+//        redBlackTree.insert(23);
 //        redBlackTree.insert(10);
-//        redBlackTree.insert(1);
-//        redBlackTree.insert(2);
-//        redBlackTree.insert(5);
-//        redBlackTree.insert(6);
-//        redBlackTree.insert(70);
-//        redBlackTree.insert(55);
-//        redBlackTree.insert(29);
-//        redBlackTree.insert(31);
-//        redBlackTree.insert(15);
-//        redBlackTree.insert(11);
-//        redBlackTree.insert(17);
 //        redBlackTree.insert(33);
-//        redBlackTree.insert(34);
-//        redBlackTree.delete(66);
-//        redBlackTree.print();
+//        redBlackTree.insert(3);
+//        redBlackTree.insert(5);
+//        redBlackTree.insert(30);
+//        redBlackTree.insert(35);
+
+        redBlackTree.insert(10);
+        redBlackTree.insert(1);
+        redBlackTree.insert(2);
+        redBlackTree.insert(5);
+        redBlackTree.insert(6);
+        redBlackTree.insert(70);
+        redBlackTree.insert(55);
+        redBlackTree.insert(29);
+        redBlackTree.insert(31);
+        redBlackTree.insert(15);
+        redBlackTree.insert(11);
+        redBlackTree.insert(17);
+        redBlackTree.insert(33);
+        redBlackTree.insert(34);
+//        redBlackTree.delete(33);
+        redBlackTree.print();
+
     }
 }
--- src/com/scalefocus/training/algorithm/search/DepthFirstSearch.java
+++ src/com/scalefocus/training/algorithm/search/DepthFirstSearch.java
@@ -8,74 +8,74 @@ import com.scalefocus.training.collection.tree.TreeNode;
  * @author Kristiyan SLavov
  */
 public class DepthFirstSearch<T extends Comparable<T>> {
-
-    /**
-     * This method traverses the tree through Depth-first search(inorder) using recursion.
-     *
-     * @param node - the roof node
-     */
-    public void inOrderTraverse(TreeNode<T> node) {
-        if (node != null) {
-            inOrderTraverse(node.getLeftChild());
-            System.out.println(node.getValue());
-            inOrderTraverse(node.getRightChild());
-        }
-    }
-
-    /**
-     * This method traverses the tree through Depth-first search(preorder) using recursion.
-     *
-     * @param node - the roof node
-     */
-    public void preOrderTraverse(TreeNode<T> node) {
-        BinarySearchTree<T> binarySearchTree = new BinarySearchTree<T>();
-        if (node != null) {
-            System.out.println(node.getValue());
-            preOrderTraverse(node.getLeftChild());
-            preOrderTraverse(node.getRightChild());
-        }
-    }
-
-    /**
-     * This method traverses the tree through Depth-first search(postorder) using recursion.
-     *
-     * @param node - the roof node
-     */
-    public void postOrderTraverse(TreeNode<T> node) {
-        if (node != null) {
-            postOrderTraverse(node.getLeftChild());
-            postOrderTraverse(node.getRightChild());
-            System.out.println(node.getValue());
-        }
-    }
-
-    /**
-     * This method traverses the tree through Depth-first search algorithm using stack.
-     * @param root - the root of the tree
-     * @param value - the value of the specified node
-     * @return - the searched node
-     */
-    public TreeNode<T> search(TreeNode<T> root, T value) {
-        if (root == null) {
-            return null;
-        }
-        Stack<TreeNode<T>> stack = new Stack<>();
-        TreeNode<T> target = null;
-        stack.push(root);
-
-        while (stack.size() != 0) {
-            TreeNode<T> currentNode = stack.poll();
-            if (currentNode.getValue().equals(value)) {
-                target = currentNode;
-                return target;
-            }
-            if (currentNode.getRightChild() != null) {
-                stack.push(currentNode.getRightChild());
-            }
-            if (currentNode.getLeftChild() != null) {
-                stack.push(currentNode.getLeftChild());
-            }
-        }
-        return null;
-    }
+//
+//    /**
+//     * This method traverses the tree through Depth-first search(inorder) using recursion.
+//     *
+//     * @param node - the roof node
+//     */
+//    public void inOrderTraverse(TreeNode<T> node) {
+//        if (node != null) {
+//            inOrderTraverse(node.getLeftChild());
+//            System.out.println(node.getValue());
+//            inOrderTraverse(node.getRightChild());
+//        }
+//    }
+//
+//    /**
+//     * This method traverses the tree through Depth-first search(preorder) using recursion.
+//     *
+//     * @param node - the roof node
+//     */
+//    public void preOrderTraverse(TreeNode<T> node) {
+//        BinarySearchTree<T> binarySearchTree = new BinarySearchTree<T>();
+//        if (node != null) {
+//            System.out.println(node.getValue());
+//            preOrderTraverse(node.getLeftChild());
+//            preOrderTraverse(node.getRightChild());
+//        }
+//    }
+//
+//    /**
+//     * This method traverses the tree through Depth-first search(postorder) using recursion.
+//     *
+//     * @param node - the roof node
+//     */
+//    public void postOrderTraverse(TreeNode<T> node) {
+//        if (node != null) {
+//            postOrderTraverse(node.getLeftChild());
+//            postOrderTraverse(node.getRightChild());
+//            System.out.println(node.getValue());
+//        }
+//    }
+//
+//    /**
+//     * This method traverses the tree through Depth-first search algorithm using stack.
+//     * @param root - the root of the tree
+//     * @param value - the value of the specified node
+//     * @return - the searched node
+//     */
+//    public TreeNode<T> search(TreeNode<T> root, T value) {
+//        if (root == null) {
+//            return null;
+//        }
+//        Stack<TreeNode<T>> stack = new Stack<>();
+//        TreeNode<T> target = null;
+//        stack.push(root);
+//
+//        while (stack.size() != 0) {
+//            TreeNode<T> currentNode = stack.poll();
+//            if (currentNode.getValue().equals(value)) {
+//                target = currentNode;
+//                return target;
+//            }
+//            if (currentNode.getRightChild() != null) {
+//                stack.push(currentNode.getRightChild());
+//            }
+//            if (currentNode.getLeftChild() != null) {
+//                stack.push(currentNode.getLeftChild());
+//            }
+//        }
+//        return null;
+//    }
 }
